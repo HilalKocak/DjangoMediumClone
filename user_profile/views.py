65f8070d72b25ebaf1ff99e01ec77419a0526ab5
Hilal38 <hhilalkocak@gmail.com>
2023-02-08 19:53:10 +0300

    django messages framework

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
 def login_view(request):
+    # Login olan kullanici direk ana sayfaya yonlendirilsin
+    if request.user.is_authenticated:
+        messages.info(request, f"{request.user.username} Daha once login olmussun")
+        return redirect('home_view')
     context=dict()
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         print(username, password)
-        user = authenticate(username, password)
+        user = authenticate(request, username=username, password=password)
+        #Bu kullanici bilgileri dogru girmis mi
         if user is not None:
         # A backend authenticated the credentials
         #Login oldugunu kullaniciya belli edelim
+            messages.add_message(request, messages.SUCCESS, 'Login oldun')
             login(request, user)
             return redirect('home_view')
         else:
