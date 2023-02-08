51aac28c9fdd966f9b645568e3acef23a584c562
Hilal38 <hhilalkocak@gmail.com>
2023-02-08 20:10:46 +0300

    logout added

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user_profile.views import login_view
+from user_profile.views import login_view, logout_view
 
 
 app_name='user_profile'
 urlpatterns = [
     path("login/", login_view,name= 'login_view'),
+    path("logout/", logout_view, name= 'logout_view'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
@@ -25,3 +25,8 @@ def login_view(request):
         else:
             print('Return invalid login error message')
     return render(request, 'user_profile/login.html', context)
+
+def logout_view(request):
+    messages.info(request, f"{request.user.username} Oturumun kapatıldı")
+    logout(request)
+    return redirect('home_view')
