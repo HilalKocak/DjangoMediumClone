04b289c6185d4792cfd8abd4da84814ea62b279f
Hilal38 <hhilalkocak@gmail.com>
2023-02-17 16:13:39 +0300

    django.core.validators usage

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,12 @@
 from .models import BlogPost
 from django import forms
 from tinymce.widgets import TinyMCE
-
+from django.core import validators
 # Create the form class.
 class BlogPostModelForm(forms.ModelForm):
     tag = forms.CharField()
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 20}))
-
+    title = forms.CharField(validators=[validators.MinLengthValidator(3, message='at least 3 characters')])
     class Meta:
         model = BlogPost
         fields = [
@@ -17,8 +17,8 @@ class BlogPostModelForm(forms.ModelForm):
             'tag', 
             ]
 
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-        if len(title) < 3:
-            raise forms.ValidationError('Ohhh at least 3 characters! ')
-        return title
+    # def clean_title(self):
+    #     title = self.cleaned_data.get('title')
+    #     if len(title) < 3:
+    #         raise forms.ValidationError('Ohhh at least 3 characters! ')
+    #     return title
